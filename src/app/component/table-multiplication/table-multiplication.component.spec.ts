cfcc6b2c1e39c13621ee0fc32471889e0132a1bb
Loïc DARRAS <darrac.loic@outlook.com>
2024-03-25 11:23:32 +0100

    first commit

---
+++ src/app/component/table-multiplication/table-multiplication.component.spec.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-table-multiplication',
+  templateUrl: './table-multiplication.component.html',
+  styleUrls: ['./table-multiplication.component.css']
+  })
+  export class TableMultiplicationComponent {
+  n: number = 2 
+  }
---
+++ src/app/component/table-multiplication/table-multiplication.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+@Component({
+  selector: 'app-table-multiplication',
+  templateUrl: './table-multiplication.component.html',
+  styleUrl: './table-multiplication.component.css'
+})
+export class TableMultiplicationComponent {
+ @Input() n: number = 2 
+}
